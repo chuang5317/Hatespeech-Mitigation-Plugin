07c5648ffa8dc6623d089c831b645bb90b572ed1
Tianyi Zuo <tz2617@arc06.doc.ic.ac.uk>
2019-10-18 16:08:48 +0100

    refactor + mutation observer

--- getText.js
+++ getText.js
@@ -1,69 +1,74 @@
+const detect_new_content = function(mutations) {
+  mutations.forEach(function(mutation) {
+    for (var i = 0; i < mutation.addedNodes.length; i++) {
+      console.log(mutation.addedNodes[i]);
+      // Create an observer instance linked to the callback function
+      const observer = new MutationObserver(detect_new_content);
+      // Start observing the target node for configured mutations
+      observer.observe(mutation.addedNodes[i], {childList: true});
+      hatespeech_detection(mutation.addedNodes[i]);
+    }
+  });
+};
+
 //visited once per page
 function walkNodeTree(root) {
-    const nodes = []; 
-    node = root;
-    start: while (node) {
-        if(node.nodeType === Node.TEXT_NODE && !['STYLE', 'SCRIPT'].includes(node.nodeName)){
-            nodes.push(node);
-        } else{
-            if (node.firstChild) {
-              node = node.firstChild;
-              continue start;
-            }
-        }
-        while (node) {
-            if (node === root) {
-                break start;
-            }
-
-            if (node.nextSibling) {
-                node = node.nextSibling;
-                continue start;
-            }
-
-            node = node.parentNode;
-        }
-    }
-    remove = [];
-    nodes.forEach((n) => {
-      p = n.parentNode.parentNode.parentNode;
-      if(nodes.includes(p)){
-        remove.push(p);
+  const nodes = []; 
+  node = root;
+  start: while (node) {
+      // Create an observer instance linked to the callback function
+      const observer = new MutationObserver(detect_new_content);
+      // Start observing the target node for configured mutations
+      observer.observe(document.body, {childList: true});
+      // Later, you can stop observing
+      // observer.disconnect();
+      if(node.nodeType === Node.TEXT_NODE && !['STYLE', 'SCRIPT'].includes(node.nodeName)){
+          nodes.push(node);
+      } else{
+          if (node.firstChild) {
+            node = node.firstChild;
+            continue start;
+          }
       }
-    });
-    return nodes.filter(function(n){
-      return !remove.includes(n);
-    });
-    // return nodes;
+      while (node) {
+          if (node === root) {
+              break start;
+          }
+
+          if (node.nextSibling) {
+              node = node.nextSibling;
+              continue start;
+          }
+
+          node = node.parentNode;
+      }
+  }
+  return nodes;
 }
 
 //TODO: 1. API for back end -- failed to remove parent nodes, use stream of text
 //2. deal with new contents on the page -- example : stack overflow expand comments, reddit 
 
-document.addEventListener('DOMContentLoaded', (event) => {
+function hatespeech_detection(root){
   function onError(error) {
     alert(`Error: ${error}`);
   }
-
   function onGot(item) {
     if(item.HateSpeechOn){
-      var style = document.createElement('style');
-      style.type = 'text/css';
-      style.innerHTML = '.blurry-text {\nfilter:blur(5px);\n}\n' + 
-                        '.blurry-text:hover {\nfilter:none;\n}';
-      document.getElementsByTagName('head')[0].appendChild(style);
-      var allText = walkNodeTree(document.body); //visit the dom
+      var allText = walkNodeTree(root); //visit the dom
       str = ""
       for (i = 0; i < allText.length; i++) {
-          str = str + allText[i];
+          str = str + allText[i].nodeValue;
+          allText[i].parentNode.style.color = getRandomColor();
       }
-      result = [50, 100, 120, 180, 234, 435, 500, 600, 700, 800];//call backend, get an array of intgers (the position of hate speech)
+      // console.log(str); //all the string on the webpage
+      result = [234, 435];//call backend, get an array of intgers (the position of hate speech)
       pos = 0;
       hateSpeechIndex = 0;
       for(i = 0; i < allText.length; i++){
           pos += allText[i].length;
           if(pos >= result[hateSpeechIndex]){
-            allText[i].parentNode.classList.add('blurry-text');
+            // allText[i].parentNode.classList.add('blurry-text');
             hateSpeechIndex++;
           }
           if(hateSpeechIndex >= result.length){
@@ -74,13 +79,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }
   var getting = browser.storage.sync.get("HateSpeechOn");
   getting.then(onGot, onError);
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  var style = document.createElement('style');
+  style.type = 'text/css';
+  style.innerHTML = '.blurry-text {\nfilter:blur(5px);\n}\n' + 
+                    '.blurry-text:hover {\nfilter:none;\n}';
+  document.getElementsByTagName('head')[0].appendChild(style);
+  hatespeech_detection(document.body);
 });
 
-// function getRandomColor() {
-//   var letters = '0123456789ABCDEF';
-//   var color = '#';
-//   for (var i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}

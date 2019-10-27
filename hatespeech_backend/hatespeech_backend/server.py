72aaefe660d00225b7d8d2d8c4fee3d03a66320c
Christopher Huang <ch5317@corona26.doc.ic.ac.uk>
2019-10-27 15:26:36 +0000

    Changed from POST to GET.

--- hatespeech_backend/hatespeech_backend/server.py
+++ hatespeech_backend/hatespeech_backend/server.py
@@ -1,14 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
 
-@app.route('/postmethod', methods=['POST'])
-def get_post_html_text():
-    htmltext = request.form['data']
-    return htmltext
+@app.route('/getmethod', methods=['GET'])
+def get_html_text():
+    htmltext = request.json
+    return jsonify({"response": "OK"})

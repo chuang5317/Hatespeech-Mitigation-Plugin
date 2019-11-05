2174d761217fedced49da45af1b16fd674cca656
Arjun Narula <arjun@Arjuns-MBP.broadband>
2019-11-05 14:18:37 +0000

    added functions to split input text into sentences and paragraphs

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,6 @@
+from nltk import tokenize
 import boto3
+
 
 #if one person is describing another person with these words, classify as hatespeech, obtained from facebook
 check = ["cunt", "dick", "asshole", "fuck", "bitch", "motherfucker", "suck my dick", "kiss my ass", "eat shit",
@@ -14,3 +16,10 @@ def syntax_analyser(text):
     #next need to go through each JSON token, see if its an adj (PartsOfSpeech, TAG), if so check if it (Text) is in the dictionary above
 
 
+#returns an array of Strings, each a sentence. Takes in a string.
+def to_sentence(text):
+    return tokenize.sent_tokenize(text)
+
+#returns an array of Strings, each a paragraph. Takes in a string.
+def to_paragraph(text):
+    return text.split("\n\n")

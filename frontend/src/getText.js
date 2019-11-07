e29617bdddee431a6396210fb3c97443094d7bf2
Ho Long Fung <hlf2817@imperial.ac.uk>
2019-11-07 17:09:25 +0000

    Add node manager ID mapping and ignore whitespace text nodes
    Node Manager is probably over engineered, for experimental purposes
    only.

--- frontend/src/getText.js
+++ frontend/src/getText.js
@@ -1,31 +1,97 @@
 /**
+ * Keeps track of DOM nodes and node ids.
+ *
+ * NOTE: this is ES6 syntax.
+ */
+class NodeManager {
+  constructor() {
+    this.nodeIDCount = 0;
+    this.nodeIDMap = new Map();
+  }
+
+  addNode(node) {
+    this.nodeIDMap.set(this.nodeIDCount, node);
+    ++this.nodeIDCount;
+  }
+
+  getNode(nodeID) {
+    return this.nodeIDMap.get(nodeID);
+  }
+
+  getID(node) {
+    // TODO: not efficient, will experiment
+    const iterator = this.nodeIDMap.keys();
+
+    for (const nodeID of this.nodeIDMap.keys()) {
+      if (this.nodeIDMap.get(nodeID) === node) {
+        return nodeID;
+      }
+    }
+    return undefined;
+  }
+}
+
+/**
+ * Initialise the node ID manager. To be called in a DOMContentLoaded listener.
+ */
+function initNodeManager() {
+  // Apparently tree walker is the fastest way to get text nodes
+  // https://stackoverflow.com/questions/2579666
+  const walker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT,
+    // NodeFilter.SHOW_ALL,
+    null,
+    false
+  );
+  let node;
+  while ((node = walker.nextNode())) {
+    nodeManager.addNode(node);
+  }
+}
+
+const nodeManager = new NodeManager();
+
+/**
  * Callback for detecting hatespeech when the DOM tree is updated.
  * @param mutations - an array of MutationRecord objects describing each change.
  */
-function detect_new_content(mutations) {
+function onNewContentAdded(mutations) {
   mutations.forEach(mutation => {
-    for (let i = 0; i < mutation.addedNodes.length; i++) {
-      hatespeech_detection(mutation.addedNodes[i]);
+    for (let node of mutation.addedNodes) {
+      nodeManager.addNode(node);
+    }
+
+    for (let node of mutation.addedNodes) {
+      hatespeech_detection(node);
     }
   });
 }
 
 /**
+ * Determine whether a node's text content is entirely whitespace.
+ * Source: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Whitespace_in_the_DOM
+ *
+ * @param node - the node to test
+ * @return - True if all of the text content of `node` is whitespace, otherwise false.
+ */
+function isAllWhiteSpace(nod) {
+  // Use ECMA-262 Edition 3 String and RegExp features
+  return !/[^\t\n\r ]/.test(nod.textContent);
+}
+
+/**
  * Traverse the DOM tree and collect text nodes.
- * @param {*} root
+ * @param root - DOM root node.
  */
 function walkNodeTree(root) {
   const nodes = [];
   node = root;
   start: while (node) {
-    if (
-      node.nodeType === Node.TEXT_NODE &&
-      !["STYLE", "SCRIPT"].includes(node.nodeName)
-    ) {
-      console.log(`${node.nodeName}, ${node.nodeType}`);
+    if (node.nodeType === Node.TEXT_NODE && !isAllWhiteSpace(node)) {
       nodes.push(node);
     } else {
-      const observer = new MutationObserver(detect_new_content);
+      const observer = new MutationObserver(onNewContentAdded);
       // Start observing the target node for configured mutations
       observer.observe(node, { childList: true });
       // Later, you can stop observing
@@ -54,15 +120,15 @@ function walkNodeTree(root) {
 /**
  * Retrieve results from the hate speech NLP service.
  *
- * Should have a parameter later for passing in the text nodes.
- * May require a callback parameter in the future (e.g. blur)
+ * @param str - string of text to send to service.
  */
-function fetchHatespeechInfo() {
+function fetchHatespeechInfo(str) {
   const apiUrl = "http://127.0.0.1:5000/getmethod";
+  // const apiUrl = "http://127.0.0.1:5000/frontend-expt";
   let xhr = new XMLHttpRequest();
 
   xhr.open("GET", apiUrl, true);
-  // xhr.send(JSON.stringify({ htmltext: str }));
+  xhr.send(JSON.stringify({ htmltext: str }));
   xhr.onprogress = () => {};
   xhr.onload = () => {
     if (xhr.status !== 200) {
@@ -109,16 +175,16 @@ function hatespeech_detection(root) {
   function onGot(item) {
     if (item.HateSpeechOn) {
       const allText = walkNodeTree(root); //visit the dom
-      console.log(`Got ${allText.length} text nodes`);
+      console.log(allText);
       let str = "";
+
       for (const textNode of allText) {
         textNode.parentNode.style.color = getRandomColor();
         str = str + textNode.nodeValue;
       }
-      console.log(str); //all the string on the webpage
 
       // Fetch the ranges to blur from the locally running service
-      fetchHatespeechInfo();
+      fetchHatespeechInfo(str);
 
       // May have to put this in the XMLHttpRequest callback: only blur onProgress.
       blurHatespeechNodes(allText);
@@ -140,8 +206,8 @@ function getRandomColor() {
   return color;
 }
 
-// Dynamically add a blur style to the document's CSS
-// Then run hatespeech.
+// Note to self: DOMContentLoaded is when the initial HTML document is completely loaded and parsed,
+// WITHOUT waiting for stylesheets, images and subframes to finish loading, as opposed to the usual "load".
 document.addEventListener("DOMContentLoaded", event => {
   var style = document.createElement("style");
   style.type = "text/css";
@@ -149,5 +215,7 @@ document.addEventListener("DOMContentLoaded", event => {
     ".blurry-text {\nfilter:blur(5px);\n}\n" +
     ".blurry-text:hover {\nfilter:none;\n}";
   document.getElementsByTagName("head")[0].appendChild(style);
+
+  initNodeManager();
   hatespeech_detection(document.body);
 });

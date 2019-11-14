08786ccfe68533796740e51696fb1a61517742dd
Matthew Hoare <matthew@mhoare.co.uk>
2019-11-14 15:53:15 +0000

    Update comprehend to use new interface

--- machine-learning/comprehend.py
+++ machine-learning/comprehend.py
@@ -11,15 +11,19 @@ check = ["cunt", "dick", "asshole", "fuck", "bitch", "motherfucker", "suck my di
          
 def analyse(text):
     comprehend = boto3.client(service_name='comprehend')
-    sentiment = comprehend.detect_sentiment(Text=text, LanguageCode='en')
-    result = False
-    
-    if (sentiment["SentimentScore"]["Negative"] > NEGATIVE_SENTIMENT_THRESHOLD):
-        #result = comprehend.detect_syntax(Text=text, LanguageCode='en')
-        result = any(word in text.lower() for word in check)
+    for node in text["nodes"]:
+        
+        sentiment = comprehend.detect_sentiment(Text=node["text"], LanguageCode='en')
+        result = False
+        
+        if (sentiment["SentimentScore"]["Negative"] > NEGATIVE_SENTIMENT_THRESHOLD):
+            #result = comprehend.detect_syntax(Text=text, LanguageCode='en')
+            result = any(word in node["text"] for word in check)
+        node["result"] = result
+        node["sentiment"] = sentiment["SentimentScore"]
     #next need to go through each JSON token, see if its an adj (PartsOfSpeech, TAG), if so check if it (Text) is in the dictionary above
-    return {"hatespeech": result, "sentiment": sentiment["SentimentScore"]}
+    return text["nodes"]
 
 def lambda_handler(event, context):
-    return analyse(event["input"])
+    return analyse(event)
 

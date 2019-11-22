9bfbc8e811b7b1a49bf3fd34914fd569c45a3731
Ho Long Fung <hlf2817@imperial.ac.uk>
2019-11-22 10:56:01 +0000

    Implement vocabulary object

--- machine-learning/expt_lstm.py
+++ machine-learning/expt_lstm.py
@@ -63,3 +63,24 @@ def preprocess(sentences):
 def tokenize(sentence):
     """ Convert a single sentence string to a list of tokens. """
     return nltk.word_tokenize(sentence)
+
+
+class Vocabulary:
+    """
+    Closed vocabulary. Maps unique tokens to integer IDs and vice versa.
+    """
+    def __init__(self):
+        self.token_to_idx = dict()
+        self.idx_to_token = list()
+
+    def add_token(self, token):
+        assert token not in self.token_to_idx
+        idx = len(self.token_to_idx)
+        self.token_to_idx[token] = idx
+        self.idx_to_token[idx] = token
+
+    def get_id(self, token):
+        """ Return integer ID of string token, None if not found. """
+        return self.token_to_idx.get(token)
+
+

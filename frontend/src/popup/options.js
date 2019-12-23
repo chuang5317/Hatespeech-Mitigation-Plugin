9137191fb1ecb098ba279932ebd08f6fcb494e8b
Eduardo Conesa-Pietscheck <ecpietscheck@Eduardos-MacBook-Air.local>
2019-12-23 16:19:15 -0400

    Included text boxes. Have yet to pass them to the backend

--- frontend/src/popup/options.js
+++ frontend/src/popup/options.js
@@ -34,6 +34,34 @@ function restoreSexismCategory() {
   sexismCheckbox.then(setCurrentChoice, onError);
 }
 
+function restoreFirstCustomCategory() {
+  function setCurrentCategory(item) {
+    firstCategory = document.getElementById("firstCategory");
+    firstCategory.value = item.firstCustomSetting;
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  var firstCustomSetting = browser.storage.sync.get("firstCustomSetting");
+  firstCustomSetting.then(setCurrentCategory, onError);
+}
+
+function restoreSecondCustomCategory() {
+  function setCurrentCategory(item) {
+    secondCategory = document.getElementById("secondCategory");
+    secondCategory.value = item.secondCustomSetting;
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  var secondCustomSetting = browser.storage.sync.get("secondCustomSetting");
+  secondCustomSetting.then(setCurrentCategory, onError);
+}
+
 var checkbox = document.getElementById("toggle-slider-input");
 checkbox.addEventListener("change", function() {
   console.log(`Hatespeech detection set to ${checkbox.checked}.`);
@@ -57,6 +85,22 @@ sexism.addEventListener("change", function() {
   });
 });
 
+var firstCategory = document.getElementById("firstCategory");
+firstCategory.addEventListener("input", function() {
+  browser.storage.sync.set({
+    firstCustomSetting: firstCategory.value
+  });
+});
+
+var secondCategory = document.getElementById("secondCategory");
+secondCategory.addEventListener("input", function() {
+  browser.storage.sync.set({
+    secondCustomSetting: secondCategory.value
+  });
+});
+
 document.addEventListener("DOMContentLoaded", restoreOptions);
 document.addEventListener("DOMContentLoaded", restoreRacismCategory);
 document.addEventListener("DOMContentLoaded", restoreSexismCategory);
+document.addEventListener("DOMContentLoaded", restoreFirstCustomCategory);
+document.addEventListener("DOMContentLoaded", restoreSecondCustomCategory);

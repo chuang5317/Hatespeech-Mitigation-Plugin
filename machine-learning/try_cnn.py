37a604b6e8c479247935e80534e4c98d020a2c99
tianyi <1448028677@qq.com>
2019-12-26 09:07:55 +0000

    uploading datasets

--- machine-learning/try_cnn.py
+++ machine-learning/try_cnn.py
@@ -15,7 +15,6 @@ sigmoid = tf.keras.layers.Dense(1, activation='sigmoid')
 model = keras.models.load_model("cnn.h5", custom_objects={'KerasLayer': embedding_layer})
 
 model.summary()
-
-print(model.predict(["sample sentence here"])) 
+print(model.predict(["Any other results?"])) 
 #CNN is not working, predicts everything as 0.9+ to get low loss and high accuracy!!
 
